e2bb5a7049a11945decb1ff1ca6c7625535c3b41
Yadder Aceituno <yadderace@gmail.com>
2020-07-13 17:44:25 -0600

    Se crean tablas para registrar el modelo y sus respectivos datos de construccion

--- SQL/DDL Tablas.sql
+++ SQL/DDL Tablas.sql
@@ -69,6 +69,54 @@ CREATE TABLE public.limpiezadata
 );
 
 
+CREATE TABLE public.modeloencabezado
+(
+    idmodelo bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    tipomodelo character varying(5) NOT NULL,
+    archivomodelo character varying(50) NOT NULL,
+	msescore float NOT NULL DEFAULT 0,
+	r2score float NOT NULL DEFAULT 0,
+	active boolean NOT NULL DEFAULT false,
+	fecharegistro timestamp without time zone DEFAULT now(),
+	CONSTRAINT construccionmodelo_pkey PRIMARY KEY (idmodelo)
+);
+
+
+CREATE TABLE public.campomodelo
+(
+    idcampomodelo bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    idmodelo bigint NOT NULL,
+    nombrecampo character varying(50) NOT NULL,
+	tipodatacampo character varying NOT NULL DEFAULT 0,
+	fecharegistro timestamp without time zone DEFAULT now(),
+	CONSTRAINT campomodelo_pkey PRIMARY KEY (idmodelo, nombrecampo),
+	CONSTRAINT campomodelo_idmodelo_fkey FOREIGN KEY (idmodelo)
+        REFERENCES public.modeloencabezado(idmodelo) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+
+CREATE TABLE public.modelodata
+(
+    idmodelodata bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+	nombrecampo character varying (50) NOT NULL,
+	idmodelo bigint NOT NULL,
+    codigoencabezado bigint NOT NULL,
+    valordata text,
+	tipodata char(1) NOT NULL,
+	fecharegistro timestamp without time zone DEFAULT now(),
+	CONSTRAINT modelodata_pkey PRIMARY KEY (idmodelodata),
+	CONSTRAINT modelodata_idmodelo_fkey FOREIGN KEY (idmodelo, nombrecampo)
+        REFERENCES public.campomodelo (idmodelo, nombrecampo) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT modelodata_codigoencabezado_fkey FOREIGN KEY (codigoencabezado)
+        REFERENCES public.encabezadoregistros(codigoencabezado) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
 create materialized view mvwSetLimpio as
 
 select codigoencabezado, 
