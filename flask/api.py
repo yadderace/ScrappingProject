222fd03dadabd0a30a8f447308619f25e6d2c56f
Yadder Aceituno <yadderace@gmail.com>
2020-07-18 12:05:36 -0600

    Se lee el directorio donde se guarda el modelo y se obtiene el modelo activo registrado en BD

--- flask/api.py
+++ flask/api.py
@@ -1,17 +1,42 @@
-from flask import Flask, request, jsonify
-import numpy as np
+import os
 import pickle 
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# Busca el modelo activo en base de datos y devuelve el nombre del archivo.
+def obtenerModeloActivo():
+    
+    engine = create_engine('postgresql://postgres:150592@localhost:5432/DBApartamentos')
+    
+    strQuery = "SELECT archivomodelo FROM modeloencabezado WHERE active = true"
+    idmodelo = engine.execute(strQuery).fetchone()[0]
+
+    return idmodelo
+
+
 @app.route('/predict', methods = ['POST'])
 def predict():
-    #directory = 'C:/Users/yadde/Documents/'
-    #model = pickle.load(open(directory + 'lr1594755397.679078.mdl', 'rb'))
-    dfParams = pd.DataFrame(request.get_json)
-    print(dfParams)
+    
+    # Obtiene directorio de las variables del sistema
+    directorioModelo = os.environ.get('MODEL_DIRECTORY')
+    print(directorioModelo)
 
-   # prediction = model.predict([np.array(list(data.values()))])
+    # Obtiene el nombre del archivo
+    strArchivoModelo = obtenerModeloActivo()
+    print(strArchivoModelo)
+
+    # Carga del modelo
+    model = pickle.load(open(directorioModelo + strArchivoModelo, 'rb'))
+    
+    # Lectura de variables del request
+    dfParams = pd.DataFrame(request.get_json())
+    print(dfParams)
+    
+    #prediction = model.predict([np.array(list(data.values()))])
     #print(prediction)
 
     #output = prediction[0]
