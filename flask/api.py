77a497f2e61ace236fff060bf537f9c6221c81d0
Yadder Aceituno <yadderace@gmail.com>
2020-07-19 23:35:19 -0600

    Se puede recibir latitud y longitud. Ordena los valores para ser procesados por el modelo.

--- flask/api.py
+++ flask/api.py
@@ -12,10 +12,20 @@ def obtenerModeloActivo():
     
     engine = create_engine('postgresql://postgres:150592@localhost:5432/DBApartamentos')
     
-    strQuery = "SELECT archivomodelo FROM modeloencabezado WHERE active = true"
-    idmodelo = engine.execute(strQuery).fetchone()[0]
+    strQuery = "SELECT archivomodelo FROM modeloencabezado WHERE active = true AND tipomodelo in ('LR', 'RF')" 
+    archivoModelo = engine.execute(strQuery).fetchone()[0]
 
-    return idmodelo
+    return archivoModelo
+
+# Busca el modelo kmeans activo en base de datos y devuelve el nombre del archivo.
+def obtenerModeloKMeansActivo():
+    
+    engine = create_engine('postgresql://postgres:150592@localhost:5432/DBApartamentos')
+    
+    strQuery = "SELECT archivomodelo FROM modeloencabezado WHERE active = true AND tipomodelo in ('KM')"
+    archivoModelo = engine.execute(strQuery).fetchone()[0]
+
+    return archivoModelo
 
 
 @app.route('/predict', methods = ['POST'])
@@ -24,29 +34,66 @@ def predict():
     # Obtiene directorio de las variables del sistema
     directorioModelo = os.environ.get('MODEL_DIRECTORY')
     
-    # Obtiene el nombre del archivo
+    # Obtiene el nombre del archivo para modelo de regresion y lo carga
     strArchivoModelo = obtenerModeloActivo()
-    
-    # Carga del modelo
-    model = pickle.load(open(directorioModelo + strArchivoModelo, 'rb'))
-    
+    modeloRegresion = pickle.load(open(directorioModelo + strArchivoModelo, 'rb'))
+
+    # Obtiene el nombre del archivo para el modelo kmeans y lo carga
+    strArchivoModelo = obtenerModeloKMeansActivo()
+    modeloKMeans = pickle.load(open(directorioModelo + strArchivoModelo, 'rb'))
+
     # Lectura de variables del request
     dfParams = pd.DataFrame(request.get_json())
     
-    dfParams['U1'] = 0
-    dfParams['U2'] = 0
-    dfParams['U3'] = 0
-    dfParams['U4'] = 0
-    dfParams['U5'] = 0
-    dfParams['U6'] = 0
-    dfParams['U7'] = 0
-    dfParams['U8'] = 0
-    dfParams['U9'] = 1
-    dfParams['U10'] = 0
+    # Utiizamos el modelo kmeans para obtener la categoria
+    categoria = modeloKMeans.predict(dfParams[['longitud', 'latitud']])
     
+    # Creamos dataframe de ubicaciones
+    dfUbicaciones = pd.DataFrame(np.zeros((len(np.unique(modeloKMeans.labels_)),), dtype=int).reshape(1,-1), 
+                columns = ["U" + str(cat) for cat in np.unique(modeloKMeans.labels_)])
+    dfUbicaciones["U" + str(categoria[0])] = 1
+    dfUbicaciones = dfUbicaciones.astype(np.uint8)
+
     print(dfParams)
 
-    prediction = model.predict(dfParams)
+    # Unimos los demas parametros
+    dfParams = pd.concat([dfParams, dfUbicaciones], axis = 1)
+
+    # Eliminamos latitud y longitud
+    dfParams = dfParams.drop(['longitud','latitud'], axis=1, errors='ignore')
+    
+    dfParams.astype({'parqueo': 'int'})
+
+    dfParams['espacio_m2'] = dfParams['espacio_m2'].astype(float)
+    dfParams['banos'] = dfParams['banos'].astype(float)
+    dfParams['habitaciones'] = dfParams['habitaciones'].astype(float)
+    dfParams[['monedaq', 'monedad', 'tipodueno', 'tipoinmobiliaria']] = dfParams[['monedaq', 'monedad', 'tipodueno', 'tipoinmobiliaria']].astype(np.uint8)
+    
+    dfParams = dfParams[['banos',	
+                        'espacio_m2',	
+                        'habitaciones',	
+                        'parqueo',	
+                        'monedaq',
+                        'monedad',	
+                        'tipodueno',	
+                        'tipoinmobiliaria',	
+                        'U0',
+                        'U1',	
+                        'U10',	
+                        'U2',	
+                        'U3',	
+                        'U4',	
+                        'U5',	
+                        'U6',	
+                        'U7',	
+                        'U8',	
+                        'U9']]
+
+    print(dfParams)
+
+    print(dfParams.dtypes)
+
+    prediction = modeloRegresion.predict(dfParams)
     print(prediction)
 
     output = prediction[0]
