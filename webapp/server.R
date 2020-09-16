772c60dbf86a240f74c111e0c566ea557ec31be1
Yadder Aceituno <yadderace@gmail.com>
2020-09-15 23:03:19 -0600

    Cambio de ubicacion de funcion de scrapping

--- webapp/server.R
+++ webapp/server.R
@@ -111,6 +111,57 @@ shinyServer(function(input, output, session) {
     
   }
   
+  fncObtenerValoresScrapping <- function(listaScrapping){
+    
+    listaResultado = list(
+      TIPO = NULL, HABITACIONES = NULL, BANOS = NULL,
+      ESPACIO_M2 = NULL, PARQUEO = NULL, PRECIO = NULL,
+      MONEDA = NULL, LATITUDE = NULL, LONGITUDE = NULL
+      
+    )
+    
+    # Verificar cada elemento del scrapping
+    for(elemento in listaScrapping){
+      
+      if(elemento$campo == "Tipo")
+        listaResultado["TIPO"] = toupper(elemento$valor)
+      
+      else if(elemento$campo == "Habitaciones")
+        listaResultado["HABITACIONES"] = as.numeric(elemento$valor)
+      
+      else if(elemento$campo == "Baños")
+        listaResultado["BANOS"] = as.numeric(elemento$valor)
+      
+      else if(elemento$campo == "Metros Cuadrados Totales")
+        listaResultado["ESPACIO_M2"] = as.numeric(str_extract(elemento$valor,"([0-9]*[.])?[0-9]+"))
+      
+      else if(elemento$campo == "Parqueadero")
+        listaResultado["PARQUEO"] = ifelse(toupper(elemento$valor) == "SI", TRUE, FALSE)
+      
+      else if(elemento$campo == "Precio"){
+        listaResultado["PRECIO"] = as.numeric(str_extract(gsub(",", "", elemento$valor),"([0-9]*[.])?[0-9]+"))
+        listaResultado["MONEDA"] = str_extract(elemento$valor,"[A-Z]+")
+      }
+      
+      else if(elemento$campo == "Latitude")
+        listaResultado["LATITUDE"] = as.numeric(elemento$valor)
+      
+      
+      else if(elemento$campo == "Longitude")
+        listaResultado["LONGITUDE"] = as.numeric(elemento$valor)
+      
+      
+      else if(elemento$campo == "Tipo de vendedor")
+        listaResultado["INMOBILIARIA"] = ifelse(toupper(elemento$valor) == "INMOBILIARIA", TRUE, FALSE)
+      
+    }
+    
+    
+    
+    return(listaResultado)
+    
+  }
+  
   fncInit()
   
   # =================================================================
